b362f90e656709acac1d5c1fb37bf49ffaf950b0
Rija Lalaina RAZAFINDRABE <rijalaraz@gmail.com>
2017-07-08 15:36:12 +0300

    change bootstrap to @3

---
+++ fleurs.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2017 at 09:33 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fleurs`
+--
+
+CREATE TABLE `fleurs` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `couleur` enum('noir','blanc') NOT NULL,
+  `prix` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fleurs`
+--
+
+INSERT INTO `fleurs` (`id`, `nom`, `couleur`, `prix`) VALUES
+(2, 'Tulipe', 'noir', 10),
+(4, 'Fuchsia', 'noir', 13),
+(8, 'Marguerite', 'blanc', 20),
+(9, 'Lobelie', 'noir', 30);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fleurs`
+--
+ALTER TABLE `fleurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fleurs`
+--
+ALTER TABLE `fleurs`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- server/db.js
+++ server/db.js
@@ -1,15 +1,10 @@
 const mysql = require('mysql');
 
 const pool  = mysql.createPool({
-    //host     : 'localhost',
-    //user     : 'root',
-    //password : '',
-    //database : 'test'
-
-    host     : 'db.dorianpharmatech.com',
-    user     : 'lalaina',
-    password : 'lalainalalaina',
-    database : 'lalaina'
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'test'
 });
 
 module.exports = pool;
