0a7ddeac7a9cf8a8cd534f55636c610f1a5aba3b
Rija Lalaina RAZAFINDRABE <rijalaraz@gmail.com>
2017-07-11 11:50:20 +0300

    db config

--- server/db.js
+++ server/db.js
@@ -1,16 +1,10 @@
 const mysql = require('mysql');
 
 const pool  = mysql.createPool({
-    host     : 'db.dorianpharmatech.com',
-    user     : 'lalaina',
-    password : 'lalainalalaina',
-    database : 'lalaina'
-
-
-    //host     : 'localhost',
-    //user     : 'root',
-    //password : '',
-    //database : 'test'
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'test'
 });
 
 module.exports = pool;
